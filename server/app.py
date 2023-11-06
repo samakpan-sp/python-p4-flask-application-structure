3ec29a262574b9d52b34162050fe98f4d3bad1f5
samakpan-sp <samakpan817@gmail.com>
2023-11-06 20:02:50 +0100

    Completed assignment 2

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello World! Welcome to my page</h1>"
+
+# @app.route('/<username>')
+# def user(username):
+#     return f'Profile for {username}'
+
+@app.route('/<student>')
+def student(student):
+    return f'Student: {student}'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
